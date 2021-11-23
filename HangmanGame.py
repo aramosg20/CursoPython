33052b99116362fc643dbbf02542dd4f76cee6f1
Aldo Ramos <aldop.ramosg@outlook.com>
2021-11-22 20:36:52 -0500

    actualizamos despùes de la contribucion de Gmail

--- HangmanGame.py
+++ HangmanGame.py
@@ -6,6 +6,7 @@ def funcionGmail():
     print("Esta es la línea que le toca al usuario de gmail")
     print("Agregamos esta línea de comando.")
     print("Gmail está de vuelta jajajajajaj.")
+    print("Revolucionamos al colocar esta línea.")
 
 
 def run():
