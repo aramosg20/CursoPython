3087f44615b9831a0e621c125fad175077bdf5ed
Aldo Ramos(auna) <aldop.ramosg@gmail.com>
2021-11-22 20:26:32 -0500

    Volvimos a contribuir en el proyecto

--- HangmanGame.py
+++ HangmanGame.py
@@ -5,6 +5,7 @@ def funcionOutlook():
 def funcionGmail():
     print("Esta es la línea que le toca al usuario de gmail")
     print("Agregamos esta línea de comando.")
+    print("Gmail está de vuelta jajajajajaj.")
 
 
 def run():
